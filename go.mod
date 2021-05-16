02d8df9ce2ea385acdb347659ed3b0cdefea42be
Kratos40-sba <a.khebchi@esi-sba.dz>
2021-05-16 11:11:58 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Kratos40-sba/steganography
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"archive/zip"
+	"image"
+	"image/jpeg"
+	"io"
+	"log"
+	"math/rand"
+	"os"
+)
+
+func generateImage(x1, y1 int) *image.RGBA {
+	im := image.NewRGBA(image.Rect(0, 0, 100, 200))
+	for pix := 0; pix < x1*y1; pix++ {
+		pixelOffset := pix * 4
+		im.Pix[0+pixelOffset] = uint8(rand.Intn(256))
+		im.Pix[1+pixelOffset] = uint8(rand.Intn(256))
+		im.Pix[2+pixelOffset] = uint8(rand.Intn(256))
+		im.Pix[3+pixelOffset] = 255
+	}
+	return im
+}
+func encodeImage(fileName string, img *image.RGBA) *os.File {
+	imageFile, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = jpeg.Encode(imageFile, img, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = imageFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return imageFile
+
+}
+func createCompressedFiles(fileName string) *os.File {
+	zipFile, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer zipFile.Close()
+	zipWriter := zip.NewWriter(zipFile)
+	var fichiers = []struct {
+		nom, cont string
+	}{
+		{"fichier1.txt", "fichier 1 "},
+		{"fichier2.txt", "fichier 2 "},
+		{"fichier3.txt", "fichier 3 "},
+	}
+	for _, fichier := range fichiers {
+		fz, err := zipWriter.Create(fichier.nom)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = fz.Write([]byte(fichier.cont))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+	err = zipWriter.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return zipFile
+}
+func hideZipFileInImage(zipFile, imageFile *os.File) *os.File {
+	f1, err := os.Open(imageFile.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f1.Close()
+	f2, err := os.Open(zipFile.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f2.Close()
+	f, err := os.Create("steganography.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = io.Copy(f, f1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = io.Copy(f, f2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return f
+}
+func detectSteganography(imageFile *os.File) bool {
+	return false
+}
+func main() {
+	imageFile := encodeImage("test.jpg", generateImage(100, 200))
+	zipFile := createCompressedFiles("test.zip")
+	steganographyFile := hideZipFileInImage(zipFile, imageFile)
+	detectSteganography(steganographyFile)
+}
